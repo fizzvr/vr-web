4e0a466f708391e68ee8b8c359be98af1a42767e
fizzvr <fizzvr@gmail.com>
2014-01-18 15:07:18 -0500

    descripcion actualizada

--- docpad.js
+++ docpad.js
@@ -7,7 +7,7 @@ docpadConfig = {
 		site: {
 			url: "http://www.fizzvr.com",
 			titulo: "FizzVR",
-			descripcion: "construyendo el mundo con tecnologías del hoy y creando bits para la web del mañana",
+			descripcion: "Compilando el mundo con tecnologías de hoy y generando bits para la web de mañana",
 			keywords: "vladimir, vladimir rodríguez, rodríguez, javascript, html5, jquery, css3, git, ajax, html, desarrolo web, diseño web, programación, v2b",
 			autor: "fizzvr",
 			email: "info@fizzvr.com"
@@ -20,9 +20,9 @@ docpadConfig = {
 		},
 		obtenerTitulo: function() {
 			if (this.document.titulo) {
-				return "" + this.site.titulo + " | " + this.document.titulo;
+				return "" + this.site.titulo + " , " + this.document.titulo;
 			} else {
-				return "" + this.site.titulo + " | Web Coder";
+				return "" + this.site.titulo + " , Programador";
 			}
 		},
 		obtenerDescripcion: function() {
