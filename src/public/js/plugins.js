d45873feda90b41641c41d76066c0a9abbcf0450
fizzvr <fizzvr@gmail.com>
2014-02-28 03:33:59 -0500

    Se termina la pagina de contacto

--- src/public/js/plugins.js
+++ src/public/js/plugins.js
@@ -1,13 +1,1 @@
-// comienza el codigo piwik
-/*var _paq = _paq || [];
-_paq.push(["trackPageView"]);
-_paq.push(["enableLinkTracking"]);
-
-(function() {
-var u=(("https:" == document.location.protocol) ? "https" : "http") + "://www.fizzvr.com/piwik/";
-_paq.push(["setTrackerUrl", u+"piwik.php"]);
-_paq.push(["setSiteId", "1"]);
-var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-})();*/
-
+// los plugins
